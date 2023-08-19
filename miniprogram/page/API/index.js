29f05714f8cf7a7dc57058996e91589d2a38c802
Meursau1T <meursault.w@outlook.com>
2023-08-20 02:46:23 +0800

    Revert "修改菜单标题、透明度、展开状态及排列"
    This reverts commit 38a9f3776f599c7ce3a2373c0cb2585aaec078fd.

--- miniprogram/page/API/index.js
+++ miniprogram/page/API/index.js
@@ -1,79 +1,91 @@
 Page({
   onShareAppMessage() {
     return {
-      title: "病禽拍拍小程序（测试版）",
-      path: "page/API/index",
-    };
+      title: '病禽拍拍小程序（测试版）',
+      path: 'page/API/index'
+    }
   },
 
   data: {
-    list: [
-      {
-        id: "media",
-        name: "病禽拍拍",
-        open: true,
-        pages: [
-          {
-            zh: "病禽标注系统",
-            url: "labelSystem/labelSystem",
-          },
-          {
-            zh: "历史记录",
-            url: "file/file",
-          },
-        ],
-      },
-    ],
+    list: [{
+      id: 'api',
+      name: '通用设置',
+      open: false,
+      pages: [{
+        zh: '微信登录',
+        url: 'login/login'
+      }, {
+        zh: '生物认证',
+        url: 'soter-authentication/soter-authentication'
+      }, {
+        zh: '设置与隐私',
+        url: 'setting/setting'
+      }]
+    }, {
+      id: 'media',
+      name: '病禽拍拍',
+      open: false,
+      pages: [{
+        zh: '病禽记录上传',
+        url: 'labelSystem/labelSystem'
+      },{
+        zh: '历史记录',
+        url: 'file/file'
+      }]
+    }],
     isSetTabBarPage: false,
-    theme: "light",
+    theme: 'light'
   },
   onLoad() {
     this.setData({
-      theme: wx.getSystemInfoSync().theme || "light",
-    });
+      theme: wx.getSystemInfoSync().theme || 'light'
+    })
 
     if (wx.onThemeChange) {
-      wx.onThemeChange(({ theme }) => {
+      wx.onThemeChange(({
+        theme
+      }) => {
         this.setData({
-          theme,
-        });
-      });
+          theme
+        })
+      })
     }
   },
   onShow() {
-    this.leaveSetTabBarPage();
+    this.leaveSetTabBarPage()
   },
   onHide() {
-    this.leaveSetTabBarPage();
+    this.leaveSetTabBarPage()
   },
   kindToggle(e) {
     const id = e.currentTarget.id;
-    const list = this.data.list;
+    const
+      list = this.data.list
     for (let i = 0, len = list.length; i < len; ++i) {
       if (list[i].id === id) {
         if (list[i].url) {
           wx.navigateTo({
-            url: `../../packageAPI/pages/${list[i].id}/${list[i].url}`,
-          });
-          return;
+            url: `../../packageAPI/pages/${list[i].id}/${list[i].url}`
+          })
+          return
         }
-        list[i].open = !list[i].open;
+        list[i].open = !list[i].open
       } else {
-        list[i].open = false;
+        list[i].open = false
       }
     }
     this.setData({
-      list,
-    });
+      list
+    })
   },
   enterSetTabBarPage() {
     this.setData({
-      isSetTabBarPage: true,
-    });
+      isSetTabBarPage: true
+    })
   },
   leaveSetTabBarPage() {
     this.setData({
-      isSetTabBarPage: false,
-    });
+      isSetTabBarPage: false
+    })
   },
-});
+})
